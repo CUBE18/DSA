bd502ee9cc4e2ba077810c6db5d3781993293223
CRG <88026296+CUBE18@users.noreply.github.com>
2021-09-06 16:04:01 +0530

    Create FirstUniqueChar.py
    Using While without DSA

---
+++ FirstUniqueChar.py
@@ -0,0 +1,12 @@
+# using while loop
+s = "charanrajgowda"
+while s != "":
+   slen0 = len(s)
+   ch = s[0]
+   s = s.replace(ch, "")
+   slen1 = len(s)
+   if slen1 == slen0-1:
+      print ("First non-repeating character is: ",ch)
+      break;
+   else:
+    print ("No Unique Character Found!")
